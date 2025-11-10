c31e9fd7243af41197c23e56c4961ae25b6ccb9e
Maya Stuttard <maya.stuttard1@nhs.net>
2025-11-10 16:00:00 +0000

    Add what do you see question

--- app/routes.js
+++ app/routes.js
@@ -18,7 +18,25 @@ router.post("/magical-powers-answer", (req, res) => {
     res.redirect("/ineligible");
   } else {
     // Send user back to the question page
-    res.redirect("/magical-powers");
+    res.redirect("/visual-check");
+  }
+});
+
+// Called when answering question about whether NHS number is known
+router.post("/see-cat-answer", (req, res) => {
+  // Make a variable and give it the value from 'nhsNumberKnown'
+  const seeCat = req.session.data.seeCat;
+
+  // Check whether the variable matches a condition
+  if (seeCat === "Pet") {
+    // Send user to a page where they'll enter their symptoms
+    res.redirect("/ineligible");
+  } else if (seeCat === "Wise-being") {
+    // Send user to a page where they are ejected
+    res.redirect("/symptoms");
+  } else {
+    // Send user back to the question page
+    res.redirect("/ineligible");
   }
 });
 module.exports = router
